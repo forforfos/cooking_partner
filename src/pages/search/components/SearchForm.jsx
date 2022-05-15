08e6e8f6a179a0c40d867680756820fa3a16dad9
Ioannis-Alexandros Papaefthymiou <alexandros.papaefthymiou@etraveligroup.com>
2022-05-15 17:46:17 +0300

    Add recipe cards to the page that will display the search results

--- src/pages/search/components/SearchForm.jsx
+++ src/pages/search/components/SearchForm.jsx
@@ -3,26 +3,83 @@ import MultipleValueTextInput from 'react-multivalue-text-input';
 import styled from "@emotion/styled";
 
 const Form = styled('form')`
-  width: 50%;
+  width: 470px;
   float: right;
+  padding: 16px 42px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  & .multiple-value-text-input label {
+  & .multiple-value-text-input {
     display: flex;
+    align-self: stretch;
 
-    .multiple-value-text-input-item-container {
-      height: 300px;
+    label {
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: end;
+
+      .multiple-value-text-input-item-container {
+        height: 250px;
+
+        & .multiple-value-text-input-item {
+          float: right;
+          padding: 6px 14px;
+          margin-bottom: 10px;
+          background: #ccc;
+          margin-right: 5px;
+          border-radius: 24px;
+          background: #a93238;
+          color: #e8d8d8;
+          box-shadow: 3px 3px 12px #333;
+        }
+      }
+
+      & input {
+        height: 40px;
+        border-radius: 30px;
+        border: none;
+        text-align: right;
+        background: #e8d8d8;
+        box-shadow: 3px 3px 12px #333;
+        text-align: left;
+        padding: 4px 20px
+      }
     }
+  }
 
-    & input {
-      height: 30px;
-      border-radius: 30px;
-      text-align: right;
+  @media screen and (max-width: 420px) {
+    width: 100%;
+
+    & .multiple-value-text-input label {
+      .multiple-value-text-input-item-container {
+        height: 200px;
+      }
     }
   }
 `;
 
-const SearchForm = () => {
-  const [ingredients, setIngredients] = useState();
+const SubmitButton = styled('input')`
+  width: 300px;
+  height: 36px;
+  text-align: center;
+  border-radius: 30px;
+  border: none;
+  font-size: 20px;
+  font-weight: 600;
+  background: #a93238;
+  color: #e8d8d8;
+  box-shadow: 3px 3px 12px #333;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    transform: scale(1.05)
+  }
+`;
+
+const SearchForm = ({ setRecipes }) => {
+  const [ingredients, setIngredients] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,15 +91,13 @@ const SearchForm = () => {
         "Content-Type": "application/json;charset=UTF-8",
       },
       body: JSON.stringify({
-        ingredients: [ingredients],
+        ingredients: ingredients,
       }),
     };
 
     fetch(url, options)
       .then(response => response.json())
-      .then(data => console.log(data));
-
-    // console.log(response.json());
+      .then(data => setRecipes(data));
   }
 
   const handleIngredientAddition = (item) => {
@@ -56,7 +111,7 @@ const SearchForm = () => {
   return (
     <Form onSubmit={handleSubmit}>
       <MultipleValueTextInput onItemDeleted={handleIngredientDeletion} onItemAdded={handleIngredientAddition}></MultipleValueTextInput>
-      <input type="submit"/>
+      <SubmitButton type="submit" value="Find Recipes"/>
     </Form>
   );
 };
--- src/pages/search/components/SearchPage.jsx
+++ src/pages/search/components/SearchPage.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import SearchForm from "./SearchForm";
 import headerImage from "../../../assets/flatlay.jpg"
+import SearchFormHeader from "./SearchFormHeader";
+import Results from "./Results";
 
 const HeaderImage = styled('div')`
   width: 100%;
@@ -8,31 +11,25 @@ const HeaderImage = styled('div')`
   background-image: url(${headerImage});
   background-position: bottom;
   background-size: cover;
+  overflow: hidden;
+
+  @media screen and (max-width: 480px) {
+    height: 100vh;
+  }
 `;
 
-const Header = styled('div')`
-  color: white;
-  text-align: right;
-  font-size: 55px;
-  margin: 30px 40px 0 0;
-`;
+const SearchPage = () => {
+  const [recipes, setRecipes] = useState([]);
 
-const SubHeader = styled('div')`
-  color: white;
-  text-align: right;
-  font-size: 20px;
-  margin: 10px 40px 0 0;
-`;
-
-const SearchPage = () => (
-  <HeaderImage>
-    <Header>Welcome to your cooking partner</Header>
-    <SubHeader>1. Enter your ingredient</SubHeader>
-    <SubHeader>2. Press ENTER</SubHeader>
-    <SubHeader>3. Do the same for all ingredients you have</SubHeader>
-    <SubHeader>4. Press the Find Recipes button</SubHeader>
-    <SearchForm />
-  </HeaderImage>
-);
+  return (
+    <>
+      <HeaderImage>
+        <SearchFormHeader />
+        <SearchForm setRecipes={setRecipes} />
+      </HeaderImage>
+      { recipes && <Results recipes={recipes} /> }
+    </>
+  )
+};
 
 export default SearchPage;
