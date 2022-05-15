f3dddc204e1fac789b1eb8f06c02b3d5baa9b62c
Ioannis-Alexandros Papaefthymiou <alexandros.papaefthymiou@etraveligroup.com>
2022-05-15 17:46:38 +0300

    Add new font family

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 import SearchPage from "../pages/search";
 import ResultsPage from "../pages/results";
 
-const Wrapper = styled('div')`
-    body {
-      margin: 0;
-      padding: 0;
-    }
-`
+const Wrapper = styled("div")`
+  margin: 0;
+  padding: 0;
+  font-family: "Mali", cursive;
+
+  & * {
+    font-family: "Mali", cursive;
+    box-sizing: border-box;
+  }
+`;
 
 const App = () => {
   return (
